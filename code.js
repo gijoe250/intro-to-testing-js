ba0239e9f3b307f9cf010687031c0ee78d036278
Joe Garcia <gijoe250@gmail.com>
2022-11-28 11:13:45 -0600

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
 // helloWorld function 
-function helloWorld() {
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// function expression syntax (assigning an anonymous function to a variable)
+const helloWorld = function() {
     return "Hello, World!";
+}
+
+function sayHello(){
+    return "Say Hello";
 }
